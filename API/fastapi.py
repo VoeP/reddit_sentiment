b9a0e8130cda165636730d32e95e0d8a2e668280
Edward Atkin <edward.atkin@live.com>
2023-09-14 16:32:01 +0100

    Allow Bert model to process comments larger than 512 characters

--- API/fastapi.py
+++ API/fastapi.py
@@ -34,7 +34,27 @@ def predict(message):
     if message is None:
         return {"error": "No comment provided"}
 
-    # Get sentiment
-    sentiment = pipeline(message)[0]
+    # Max length is 512 so split the message into chunks
+    chunk_size = 512
+    sentiments = []
+    confidences = []
+    for i in range(0, len(message), chunk_size):
+        chunk = message[i:i+chunk_size]
+        # Get sentiment
+        sentiment = pipeline(chunk)[0]
+        # Add to the list
+        sentiments.append(sentiment['label'])
+        confidences.append(sentiment['score'])
+
+    print("Processed chunks: ", len(sentiments))
+
+    # Return the average sentiment and confidence
+    # Sentiment is of a certain form so we need to modify it a little
+    # For the Bert model it is "n stars" so take the first char and convert to int
+    avg_sentiment = round(sum([int(s[0]) for s in sentiments]) / len(sentiments))
+    # Convert back to the "n stars" format
+    sentiment = str(avg_sentiment) + " stars"
+    avg_confidence = sum(confidences) / len(confidences)
+
     # There will be some predict code here in future but for now just return a random number
-    return {"comment": message, "sentiment": sentiment['label'], "confidence": sentiment['score']}
+    return {"comment": message, "sentiment": sentiment, "confidence": avg_confidence}
