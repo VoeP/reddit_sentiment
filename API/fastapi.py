eab01d973fe2ced35b0086125b4d73d19c9d9e9a
Edward Atkin <edward.atkin@live.com>
2023-09-14 15:45:36 +0100

    Add Bert model to the API for better text classification

--- API/fastapi.py
+++ API/fastapi.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, TextClassificationPipeline
 
 app = FastAPI()
 
-# Load the 'toxic comment' model from Hugging Face
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, TextClassificationPipeline
+# Set up some models so we can pick what we want to use
+def load_toxic_comment_model():
+    # Load the 'toxic comment' model from Hugging Face
+    model_path = "martin-ha/toxic-comment-model"
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
 
-model_path = "martin-ha/toxic-comment-model"
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForSequenceClassification.from_pretrained(model_path)
+    return TextClassificationPipeline(model=model, tokenizer=tokenizer)
 
-pipeline =  TextClassificationPipeline(model=model, tokenizer=tokenizer)
+def load_BERT_model():
+    # Load the BERT model
+    model_path = 'nlptown/bert-base-multilingual-uncased-sentiment'
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+    return TextClassificationPipeline(model=model, tokenizer=tokenizer)
+
+pipeline = load_BERT_model()
 
 # Create the root endpoint
 @app.get("/")
