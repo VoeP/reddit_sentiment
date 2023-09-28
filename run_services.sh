00f8380f288ff090428ab4b6b744e98d356a35c3
Edward Atkin <edward.atkin@live.com>
2023-09-28 20:46:05 +0100

    Update dockerfile with environment variables. Update run_services.sh to print credentials which can be used as a verification step

--- run_services.sh
+++ run_services.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# First run the print_credentials script which will verify that the environment variables are correctly configured
+echo "Please verify these credentials are as expected otherwise data collection will not work"
+python scripts/print_credentials.py
+
 uvicorn API.fastapi:app --host 0.0.0.0 &
 python scripts/scrape_reddit_data.py
