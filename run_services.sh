fa18c1860f0d0699d182418ce3c32b5cd1c014a8
Edward Atkin <edward.atkin@live.com>
2023-09-30 18:12:49 +0100

    Update dockerfile and shell script so requirements.txt is installed upon container first startup instead of during build.

--- run_services.sh
+++ run_services.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Install packages
+# We do this here because otherwise it makes the docker image fucking massive
+RUN pip install -r requirements.txt
+
 # First run the print_credentials script which will verify that the environment variables are correctly configured
 echo "Please verify these credentials are as expected otherwise data collection will not work"
 python scripts/print_credentials.py
 
-uvicorn API.fastapi:app --host 0.0.0.0 &
+uvicorn API.fastapi:app --host 0.0.0.0 --port $PORT &
 python scripts/scrape_reddit_data.py
