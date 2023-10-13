32a982cb44638b3e0ba3faac95d56378d546dac0
Edward Atkin <edward.atkin@live.com>
2023-10-13 07:48:21 +0100

    Update initialise bert to include the emotions model

--- scripts/initialise_bert.py
+++ scripts/initialise_bert.py
@@ -1,4 +1,4 @@
-"""Single purpose script to load a Bert model
+"""Single purpose script to load a Bert model and the emotions model
 Bert requires additional downloads to work
 Running this during container image building will ensure that the model requirements are pre-downloaded"""
 import torch
@@ -11,4 +11,13 @@ def load_huggingface_model(model_path):
 
     return TextClassificationPipeline(model=model, tokenizer=tokenizer)
 
+# Generic function for getting tokenizer and model from huggingface
+def load_huggingface_tokenizer_model(model_path):
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+    return tokenizer, model
+
+
 pipeline_bert = load_huggingface_model("nlptown/bert-base-multilingual-uncased-sentiment")
+emotions_tokeniser, emotions_model = load_huggingface_tokenizer_model("cardiffnlp/twitter-roberta-base-emotion")
