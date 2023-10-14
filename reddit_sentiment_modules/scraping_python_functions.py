e056c5744904ee7e0eabb66db936c9eac9c156ae
Edward Atkin <edward.atkin@live.com>
2023-10-14 12:06:32 +0100

    Update initialise bert script to save models

--- reddit_sentiment_modules/scraping_python_functions.py
+++ reddit_sentiment_modules/scraping_python_functions.py
@@ -155,15 +155,15 @@ def process_data(data, text_column="text", tokenizer1=None, model1=None, tokeniz
 
     # Generic function for loading a huggingface model
     def load_huggingface_model(model_path):
-        tokenizer = AutoTokenizer.from_pretrained(model_path, cache_dir="/app/cache")
-        model = AutoModelForSequenceClassification.from_pretrained(model_path, cache_dir="/app/cache")
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        model = AutoModelForSequenceClassification.from_pretrained(model_path)
 
         return TextClassificationPipeline(model=model, tokenizer=tokenizer)
 
     # Generic function for getting tokenizer and model from huggingface
     def load_huggingface_tokenizer_model(model_path):
-        tokenizer = AutoTokenizer.from_pretrained(model_path, cache_dir="/app/cache")
-        model = AutoModelForSequenceClassification.from_pretrained(model_path, cache_dir="/app/cache")
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        model = AutoModelForSequenceClassification.from_pretrained(model_path)
 
         return tokenizer, model
 
